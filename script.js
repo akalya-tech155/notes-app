bc3c93beb1cd58ee1ba95cd89421e9946d1c50ad
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-13 16:54:42 +0530

    updated js code

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function addNote() {
     console.log("Add Note");
 }
+
+
+
+// adeed new js code based on (local-storage)
+
+let notes = [];
+
+function addNote() {
+
+    const noteText =
+        document.getElementById("noteInput").value;
+
+    notes.push(noteText);
+
+    localStorage.setItem(
+        "notes",
+        JSON.stringify(notes)
+    );
+}
