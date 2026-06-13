ef85880fd3d3ee901c2d073a799c33d97a0933a9
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-13 17:24:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,60 +5,12 @@ function addNote() {
 
 
 
-// added new js code based on (delete note)
-
-//added new js file based on (search-notes)
-
-function searchNotes() {
-
-    const search =
-        document.getElementById("searchInput")
-        .value
-        .toLowerCase();
-
-    const notes =
-        document.querySelectorAll(".note");
-
-    notes.forEach(note => {
-
-        if (
-            note.innerText
-            .toLowerCase()
-            .includes(search)
-        ) {
-            note.style.display = "block";
-        } else {
-            note.style.display = "none";
-        }
-
-    });
 
 
-// add new js code based on (create note)
 
 
-function addNote() {
-
-    const noteText =
-        document.getElementById("noteInput").value;
-
-    const note =
-        document.createElement("div");
-
-    note.className = "note";
-
-    note.innerHTML =
-        `${noteText}
-         <button onclick="this.parentElement.remove()">
-            Delete
-         </button>`;
-
-    note.innerText = noteText;
 
 
-    document
-        .getElementById("notesContainer")
-        .appendChild(note);
 
 
-}
+
