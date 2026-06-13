d4e018c7298ef627246b33e4ee478eb7b660725f
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-13 17:08:23 +0530

    updated file

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 function addNote() {
     console.log("Add Note");
 }
+
+
+//added new js file based on (search-notes)
+
+function searchNotes() {
+
+    const search =
+        document.getElementById("searchInput")
+        .value
+        .toLowerCase();
+
+    const notes =
+        document.querySelectorAll(".note");
+
+    notes.forEach(note => {
+
+        if (
+            note.innerText
+            .toLowerCase()
+            .includes(search)
+        ) {
+            note.style.display = "block";
+        } else {
+            note.style.display = "none";
+        }
+
+    });
+}
