9923f2933017643b9e18c29c9d963e744758e725
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-13 16:51:55 +0530

    updated js code

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 function addNote() {
     console.log("Add Note");
 }
+
+
+
+// added new js code based on (delete note)
+
+function addNote() {
+
+    const noteText =
+        document.getElementById("noteInput").value;
+
+    const note =
+        document.createElement("div");
+
+    note.className = "note";
+
+    note.innerHTML =
+        `${noteText}
+         <button onclick="this.parentElement.remove()">
+            Delete
+         </button>`;
+
+    document
+        .getElementById("notesContainer")
+        .appendChild(note);
+}
